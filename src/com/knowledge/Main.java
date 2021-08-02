f02eb40d0f99ff7ae8c989ccc7a7ad190b52beb5
EtherealSymbol <2391892926@qq.com>
2021-08-02 10:19:02 +0800

    Create Main.java

---
+++ src/com/knowledge/Main.java
@@ -0,0 +1,14 @@
+package com.knowledge;
+
+public class Main {
+
+    static {
+        int a = 10;
+        System.out.println("静态代码块中的a = "+a);
+    }
+
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//        Main m = new Main();
+    }
+}
